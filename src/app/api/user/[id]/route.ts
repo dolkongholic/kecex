9d1cc31034935e79c563789030fad95f4f618379
unknown <dolkongholic@gmail.com>
2023-10-11 09:05:07 +0900

    수정3

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,34 +1,21 @@
-import { verifyJwt } from '@/app/lib/jwt'
-import prisma from '@/app/lib/prisma'
+import { verifyJwt } from "@/app/lib/jwt";
+import prisma from "@/app/lib/prisma";
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } },
+  { params }: { params: { id: string } }
 ) {
   // 추가된 부분
-  const accessToken = request.headers.get('authorization')
+  const accessToken = request.headers.get("authorization");
   if (!accessToken || !verifyJwt(accessToken)) {
-    return new Response(JSON.stringify({ error: 'No Authorization' }), {
+    return new Response(JSON.stringify({ error: "No Authorization" }), {
       status: 401,
-    })
+    });
   }
 
-  console.log(params)
+  console.log(params);
 
-  const id = Number(params.id)
+  const id = Number(params.id);
 
-  const userPosts = await prisma.post.findMany({
-    where: {
-      authorId: id,
-    },
-    include: {
-      author: {
-        select: {
-          email: true,
-          name: true,
-        },
-      },
-    },
-  })
-  return new Response(JSON.stringify("ok"))
+  return new Response(JSON.stringify("ok"));
 }
