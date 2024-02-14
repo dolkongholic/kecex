ed49576b3be7aeaf78a88b3f8d6208e317f9aaa7
unknown <dolkongholic@gmail.com>
2024-02-14 09:26:32 +0900

    수정05

--- app/action/getQna.ts
+++ app/action/getQna.ts
@@ -1,12 +1,15 @@
 import prisma from "@/app/libs/prisma";
 
 interface IParams {
-  email?: string;
+  email: string | null;
 }
 
 export default async function getQna(params: IParams) {
   const { email } = params;
 
+  if (!email) {
+    return;
+  }
   const qna = await prisma.qna.findMany({
     where: {
       email: email,
