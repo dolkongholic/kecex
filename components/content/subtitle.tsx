956257c46e631b40cd070b521ab1fca0e746b3e3
sunyoung337 <149979737+sunyoung337@users.noreply.github.com>
2024-03-21 17:33:06 +0900

    3/21 sub 일부 수정 및 메인 오류 수정

--- components/content/subtitle.tsx
+++ components/content/subtitle.tsx
@@ -8,10 +8,11 @@ type Props = {
 export default function ContentSubTitle({ title }: Props) {
   return (
     <>
-      <div className="w-full h-[50px] flex justify-start items-center text-subtitle border-b-[1px] text-[18px] md:text-[16px] border-slate-500 text-black mb-[20px] md:px-[20px]">
-        <div className="mr-[5px] md:mr-[10px]">
+      <div className="w-full flex justify-start items-center text-[18px] md:text-[20px] 
+      text-black mb-[15px] mt-[40px]">
+        {/* <div className="mr-[5px] md:mr-[10px]">
           <Image src={PicSubTitleIcon} alt="icon" width={20} height={20} />
-        </div>
+        </div> */}
         <div>{title}</div>
       </div>
     </>
